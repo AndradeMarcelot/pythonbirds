4fb10e45e6b79f9d8f52b496fe35af3c4dad64b9
Marcelo Andrade <a.marcelo13@yahoo.com.br>
2021-02-06 01:13:39 -0300

    criando classe Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,14 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 30):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Marcelo', 35)
     print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
+    print(p.nome)
+    print(p.idade)
